f258700779de6ff64d59b9104d2d46fb34291112
Leo78923 <117394079+Leo78923@users.noreply.github.com>
2023-08-31 17:28:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+
 function setup()
 {
     video = createCapture(VIDEO);
-    video.size(450, 400);
-    canvas = createCanvas(450, 400);
+    video.size(350, 300);
+    canvas = createCanvas(550, 500);
     canvas.position(560, 125);
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -16,13 +21,23 @@ function modelLoaded()
 
 function gotPoses(results)
 {
-    if(results.lengh > 0)
+    if(results.length > 0)
     {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x; 
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + "difference = " + difference);
     }
 }
 
 function draw()
 {
     background('#8515ff');
+    document.getElementById("font_size").innerHTML = "O tamanho da fonte será de = " + difference + "px";
+    textSize(difference);
+    fill('#1cfb4b');
+    text('Leo',50, 400);
 }
